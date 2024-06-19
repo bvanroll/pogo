da23fc66aae62d0dc5115add372aa9828393c008
beppe <beppe.vanrolleghem@gmail.com>
2024-06-19 17:40:21 +0200

    mmm

---
+++ player_cam.gd
@@ -0,0 +1,19 @@
+extends Camera3D
+@onready
+var player = $"../player"
+
+const cam_speed = 20.0
+const cam_distance = 2.0
+const player_height = 2.0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.y = move_toward(position.y, player.position.y + cam_distance + player_height/2., cam_speed * delta)
+	position.z = move_toward(position.z, player.position.z + cam_distance, cam_speed*delta)
+	position.x = move_toward(position.x, player.position.x, cam_speed* delta)
+	look_at(player.position + Vector3(0., player_height, 0.))
+	pass
--- player_move.gd
+++ player_move.gd
@@ -4,8 +4,10 @@ extends CharacterBody3D
 const SPEED = 5.0
 const AIRSPEED = 8.0
 const MAXSPEED = 8.0
-const JUMP_VELOCITY = 3.5
-
+const JUMP_VELOCITY = 8.5
+const BHOPDIFF = 8.0 #difficulty for bhops aka time before the rebounce height is reduced to 0
+const BHOPHEIGHTDIV = 3. #divides the rebounce height, 1 for perfect rebounce
+var max_neg_vel = 0.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -14,7 +16,7 @@ func _physics_process(delta):
 	# Add the gravity.
 
 	# Handle jump.
-	var jv = JUMP_VELOCITY + abs(velocity.y)
+
 	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 	#	velocity.y = jv
 
@@ -22,24 +24,30 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-
+	var direction = (transform.basis * Vector3(0, 0, input_dir.y)).normalized()
+	var rotation = input_dir.x;
 	if is_on_floor():
+		var jv = JUMP_VELOCITY + (abs(velocity.y) *2.)
 		if direction:
 			if abs(velocity.x) < MAXSPEED:
 				velocity.x += direction.x * SPEED *3. * delta
 			if abs(velocity.z) < MAXSPEED:
 				velocity.z += direction.z * SPEED *3. * delta
-		if Input.is_action_pressed("ui_accept"):
-			velocity.y += jv
+		if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("jump"):
+			velocity.y += jv + abs(max_neg_vel/BHOPHEIGHTDIV)
+		
+		max_neg_vel = move_toward(max_neg_vel, 0, SPEED * (delta*BHOPDIFF))
 		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
 		velocity.z = move_toward(velocity.z, 0, SPEED * delta)
 	
 	else:
 		velocity.y -= gravity * delta
+		if velocity.y < max_neg_vel:
+			max_neg_vel = velocity.y
 		if direction:
 			velocity.x += direction.x * AIRSPEED *3. * delta
 			velocity.z += direction.z * AIRSPEED *3. * delta
+			rotate_y(input_dir.x)
 		velocity.x = move_toward(velocity.x, 0, SPEED * (delta/2.))
 		velocity.z = move_toward(velocity.z, 0, SPEED * (delta/2.))
 		log(1.)
